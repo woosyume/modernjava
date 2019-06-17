a68d44267b7d56b028a0f582f6825e3b009d1375
Woohyeok Kim, Aaron <woohyeok.kim@rakuten.com>
2019-06-17 21:28:23 +0900

    Base : Function

---
+++ 190617/src/app/FunctionalInterfaceExample.java
@@ -0,0 +1,25 @@
+package app;
+
+import java.util.function.Function;
+
+/**
+ * FunctionalInterfaceExample
+ */
+public class FunctionalInterfaceExample {
+    public static void main(String[] args) {
+        // 인터페이스인데 그 안에 추상인터페이스가 하나만 존재.
+        // 람다 표현식을 사용하기 위해 필요한 인터페이스
+        // 람다 표현식의 타입이 함수형 인터페이스
+        // identity function : 인풋과 같은 타입의 리턴(변형없이 그대로)
+        
+        Function<String, Integer> toInt = new Function<String,Integer>() {
+            @Override
+            public Integer apply(final String value) {
+                return Integer.parseInt(value);
+            }
+        };
+
+        int number = toInt.apply("10");
+        System.out.println(number);
+    }
+}
