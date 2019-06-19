459dcb19629da9294daa5ffb3b082b4022136405
Woohyeok Kim, Aaron <woohyeok.kim@rakuten.com>
2019-06-20 08:50:36 +0900

    Before Lombok

---
+++ 190620FunctionalInterfacePractice/src/app/FunctionalInterfaceExample.java
@@ -0,0 +1,74 @@
+package app;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * FunctionalInterfaceExample
+ */
+public class FunctionalInterfaceExample {
+    
+}
+
+class Product {
+    private long id;
+    private String name;
+    private BigDecimal price;
+
+    public Product() {}
+
+    public Product(long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", price='" + getPrice() + "'" +
+            "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Product)) {
+            return false;
+        }
+        Product product = (Product) o;
+        return id == product.id && Objects.equals(name, product.name) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+}
