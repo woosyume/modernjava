7ffe95f6b75c4f5006889b6285437f6fe6658c63
Woohyeok Kim, Aaron <woohyeok.kim@rakuten.com>
2019-06-20 08:33:16 +0900

    Cannot use Generic as functional interface

--- 190620FunctionalInterfacePractice/src/app/App.java
+++ 190620FunctionalInterfacePractice/src/app/App.java
@@ -6,6 +6,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         BigDecialToCurrency currency = bd -> "$" + bd.toString(); // Can infer the type from functional interface.
         System.out.println(currency.toCurrency(new BigDecimal("120.00")));
+
+        final invalidFunctionalInterface invalid = value -> value.toString(); // 타입 지정이 없기 
     }
 
     /**
@@ -14,5 +16,10 @@ public class App {
     @FunctionalInterface
     public interface BigDecialToCurrency {
         String toCurrency(BigDecimal value);
+    }
+
+    @FunctionalInterface // 람다 표현식을 사용할 수 없는 예
+    public interface invalidFunctionalInterface {
+       <T> String translate(T value);
     }
 }
